5b3ad296852a64d63494ab0682d5d0690e2a3849
ndizeyedavid <davidndizeye101@gmail.com>
2026-05-09 14:16:08 +0200

    chore: installed and configured tailwindcss

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,8 +1,8 @@
-import React from "react";
 export default function App() {
   return (
     <div>
-      <p>App Mellow</p>
+      <p className="text-lg">App Mellow</p>
+      <div className="size-52 bg-radial from-blue-600 to-purple-500 p-2 relative"></div>
     </div>
   );
 }
